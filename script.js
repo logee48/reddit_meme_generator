a2563180a66e60525ad6204bf3e8a6dfcb612d83
jesper <logeshe9159@gmail.com>
2022-11-10 13:47:23 +0530

    .

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ async function loadNames() {
   const response = await fetch('http://meme-api.herokuapp.com/gimme');
   const names = await response.json();
   console.log(names);
+  document.getElementById('subredditplace').innerHTML = names.subreddit;
   document.getElementById('imageitself').src = names.url;
   document.getElementById('captionplace').innerHTML = names.title;
 }
